7fb9bcd610403dd9d0f588c835dcd8644d2eb3db
Renukaresi <renukaresi7@gmail.com>
2019-01-08 15:57:00 +0530

    Even or Odd

---
+++ Program 1/Program 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EvenorOdd(2));
+
+        }
+        public static bool EvenorOdd(int n)
+        {
+            bool flag = true;
+            if(n%2==0)
+            {
+                return flag;
+            }
+                       
+                return false;
+            
+  
+        }
+    }
+}
